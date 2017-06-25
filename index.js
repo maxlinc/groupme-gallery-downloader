c7ef2ec8af19c97d4fb2a601d36161302eb6eff2
Max Lincoln <max@devopsy.com>
2017-06-24 22:53:27 -0400

    Quick & dirty changes for downloading galleries from all groups

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import inquirer from 'inquirer';
+import find_groups from './lib/find_groups';
 import connecter from './lib/connecter';
 
 /**
@@ -13,14 +14,6 @@ let questions = [
     validate: (input) => {
       return input.length >= 40 ? true : 'Tokens should be at least 40 characters.'
     }
-  },
-  {
-    type: 'input',
-    name: 'group_id',
-    message: 'What is your GroupMe Group\'s ID?',
-    validate: (input) => {
-      return input.length >= 5 && /^\d+$/.test(input) ? true : 'Please input a valid GroupMe ID Number'
-    }
   }
 ];
 
@@ -28,7 +21,15 @@ let questions = [
  * Inquirer instantiation
  */
 inquirer.prompt(questions,
-  function ({auth_token, group_id }) {
-    connecter(auth_token, group_id);
+  function ({auth_token}) {
+    find_groups(auth_token).then(function (group_ids) {
+      for (let i = 0; i < group_ids.length; i++) {
+        let group_id = group_ids[i];
+        console.log("Download gallery for group_id: " + group_id);
+        connecter(auth_token, group_id);
+      }
+    }).catch(function (error) {
+       console.log(error.message);
+    });
   }
 );
